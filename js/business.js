10934e801a4dcd87848fb53905f4ee6ab4b74ed5
dudrl57412-spec <dudrl57412@gmail.com>
2026-04-17 11:44:35 +0900

    소개,비즈니스 영역 완성

---
+++ js/business.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+  const swBusiness = new Swiper(".sw-business", {
+    slidesPerView: 1,
+    //spaceBetween: 20, <- slide 간의 간격(gap)
+    breakpoints: {
+      //480~1023
+      480: { slidesPerView: 2 },
+      //1024픽셀 이후
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
