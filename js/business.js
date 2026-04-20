cf55e1ad9fa50932e109747bcd6fd2be1c1879fe
dudrl57412-spec <dudrl57412@gmail.com>
2026-04-20 12:16:19 +0900

    서비스,하단,gotop,반응형,waypoint적용 / visual 스와이퍼 기능 추가예정

--- js/business.js
+++ js/business.js
@@ -3,12 +3,19 @@ window.addEventListener("load", () => {
     slidesPerView: 1,
     //spaceBetween: 20, <- slide 간의 간격(gap)
     breakpoints: {
-      //480~1023
+      //640~1023
       480: { slidesPerView: 2 },
       //1024픽셀 이후
       1024: {
         slidesPerView: 3,
       },
     },
+  });
+  // business-modal
+  const businessModal = document.querySelector(".business-modal");
+  businessModal.addEventListener("click", () => {
+    console.log("click");
+
+    businessModal.style.display = "none";
   });
 });
